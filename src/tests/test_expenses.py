ad298a9af1f5839ffa560b0e5cf4e0526adca68b
Brenden Hyde <brendenahyde@gmail.com>
2023-02-25 15:28:29 -0800

    Add more tests for Expense addition

--- src/tests/test_expenses.py
+++ src/tests/test_expenses.py
@@ -2,5 +2,36 @@ from burndown.entities import *
 
 
 def test_expense_totals():
+    """Ensure the Expense.total_cost() method works."""
     e = Expense(name="Manga", amount=7.99)
     assert e.total_cost() == 7.99
+
+
+def test_expense_totals_multiple_days():
+    """Ensure the Expense.total_cost() method works with multiple days."""
+    e = Expense(name="Funyuns", amount=3.99, _days_of_month=set([1, 15, 31]))
+    assert e.total_cost() == 3.99 * 3
+
+
+def test_expense_addition_commutative():
+    """Ensure that addition is commutative between expenses."""
+    e1 = Expense(name="Headphones", amount=50.00)
+    e2 = Expense(name="Laptop", amount=800.00)
+    radd = e1 + e2
+    ladd = e2 + e1
+    assert radd == ladd
+
+
+def test_expense_addition_commutative_multiple():
+    """Ensure that addition is commutative between expenses with >1 days."""
+    e1 = Expense(name="Gardettos", amount=5.00, _days_of_month=set([1, 2, 3]))
+    e2 = Expense(name="Laptop", amount=800.00)
+    radd1 = e1 + e2
+    ladd1 = e2 + e1
+    assert radd1 == ladd1
+
+    e3 = Expense(name="Pencil", amount=3.00, _days_of_month=set([1, 2, 3]))
+    e4 = Expense(name="Coffee", amount=3.00, _days_of_month=set([1, 2, 3]))
+    radd2 = e3 + e4
+    ladd2 = e4 + e3
+    assert radd2 == ladd2
