c6305e092338e14c20a7d39d08bf9c27a33853ea
Brenden Hyde <brendenahyde@gmail.com>
2023-02-25 14:49:58 -0800

    make expense addition commutative

--- src/burndown/entities/__init__.py
+++ src/burndown/entities/__init__.py
@@ -14,16 +14,40 @@ class Expense:
     name: str
     amount: float = 0.0
     active: bool = True
-    days_of_month: Set = field(default_factory=set)
-    frequency: int = 1
+    _days_of_month: Set = field(default_factory=lambda: set([1]))
+
+    @property
+    def days_of_month(self):
+        return self._days_of_month
+
+    @days_of_month.setter
+    def days_of_month(self, new_day):
+        try:
+            assert isinstance(new_day, int)
+            assert new_day in range(1, 32)
+            self._days_of_month.add(new_day)
+        except AssertionError:
+            print("Illegal day of month. Not setting.")
+
+    def total_cost(self):
+        if self.active:
+            return self.amount * len(self.days_of_month)
+        else:
+            return 0.0
 
     def __add__(self, other):
         assert isinstance(other, Expense)
-        return self.amount + other.amount
+        return self.total_cost() + other.total_cost()
 
     def __radd__(self, other):
         if other == 0:
-            return self
+            return self.total_cost()
+        else:
+            return self.__add__(other)
+
+    def __ladd__(self, other):
+        if other == 0:
+            return self.total_cost()
         else:
             return self.__add__(other)
 
@@ -46,4 +70,4 @@ class Year:
         return sum([month.total() for month in self.months])
 
 
-__all__ = ["Expense", "Month"]
+__all__ = ["Expense", "Month", "Year"]
