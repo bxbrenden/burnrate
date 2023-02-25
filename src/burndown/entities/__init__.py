5b6a89f2d98b076861698bcf00c11956ee2e6b4f
Brenden Hyde <brendenahyde@gmail.com>
2023-02-25 14:25:10 -0800

    start defining main classes

--- src/burndown/entities/__init__.py
+++ src/burndown/entities/__init__.py
@@ -1,14 +1,49 @@
+from dataclasses import dataclass, field
+from typing import List, Set
+
+
+@dataclass
+class Account:
+    name: str
+    acct_type: str
+    balance: float
+
+
+@dataclass
 class Expense:
-    def __init__(self, name, account, amount=0.0, active=True, days_of_month=[1], frequency=1):
-        pass
+    name: str
+    amount: float = 0.0
+    active: bool = True
+    days_of_month: Set = field(default_factory=set)
+    frequency: int = 1
+
+    def __add__(self, other):
+        assert isinstance(other, Expense)
+        return self.amount + other.amount
+
+    def __radd__(self, other):
+        if other == 0:
+            return self
+        else:
+            return self.__add__(other)
 
 
+@dataclass
 class Month:
-    def __init__(self, name):
-        self.name = name
-        self.expenses = []
+    name: str
+    expenses: List[Expense] = field(default_factory=list)
 
     def total(self):
         return sum(self.expenses)
 
-__all__ = ['Expense', 'Month']
+
+@dataclass
+class Year:
+    year: int
+    months: List[Month] = field(default_factory=list)
+
+    def total(self):
+        return sum([month.total() for month in self.months])
+
+
+__all__ = ["Expense", "Month"]
